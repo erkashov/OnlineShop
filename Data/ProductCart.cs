6a9a7ef0a9f3180c59c88d2f6011d3c153c6e4b2
erkashov <emir2018k@yandex.ru>
2023-02-25 15:34:48 +0300

    Cart

---
+++ Data/ProductCart.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Data
+{
+    public class ProductCart
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Count { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
