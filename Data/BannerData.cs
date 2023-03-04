b3bb926c7ec4a594eb812d83725a8ffa7b355d47
erkashov <emir2018k@yandex.ru>
2023-03-04 15:51:50 +0300

    banners

---
+++ Data/BannerData.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineShop.Data
+{
+    public class BannerData
+    {
+        public string Header { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageLink { get; set; }
+
+        public string Link { get; set; }
+
+        public string LinkHeader { get; set; }
+        public BannerData()
+        {
+            Header = "Заголовок";
+            Description = "Описание";
+            ImageLink = "";
+            Link = "/";
+            LinkHeader = "Смотреть";
+        }
+    }
+}
