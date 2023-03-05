74c557ff111b6b4c497e508e1f30465b4e76b3ab
erkashov <emir2018k@yandex.ru>
2023-03-05 11:18:01 +0300

    black shopping cart

--- Data/ProductCart.cs
+++ Data/ProductCart.cs
@@ -9,5 +9,19 @@ namespace OnlineShop.Data
         public int Count { get; set; }
         public Product Product { get; set; }
         public string ImagePath { get; set; }
+        public double Price
+        {
+            get
+            {
+                return Product.Prices != null ? Product.Prices.FirstOrDefault().price_nal : 0;
+            }
+        }
+        public double Summa
+        {
+            get
+            {
+                return Price * Count;
+            }
+        }
     }
 }
